0eb429b01989143aee91adef7a12481260ec6ff6
Ammar Ahmed Khan <161769688+ammarakk@users.noreply.github.com>
2024-04-03 20:29:54 +0500

    created

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+const pakistaniFoodMenu: string[] = [
+    "Biryani","Nihari","Haleem","Karhai","Chapli Kebab","Samosa","Pakora","Seekh Kebab","Paratha","Naan"];
+
+console.log(chalk.green.bold("Pakistani Food Menu:"));
+pakistaniFoodMenu.forEach(item => {
+    console.log(chalk.blue(item));
+});
+
+
+const main = async () => {
+    let todos = [];
+    let condition = true;
+
+    while (condition) {
+        let addtasks = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "input",
+                message: "What do you want to order",
+            },
+            {
+                name: "addmore",
+                type: "confirm",
+                message: "Do you want to add more orders",
+                default: false, // default should be a boolean
+            },
+        ]);
+
+        todos.push(addtasks.todo);
+        condition = addtasks.addmore; // Update condition based on user input
+    }
+    console.log(todos);
+};
+
+main();
+
+
